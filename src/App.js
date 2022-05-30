5eb02e5e99a11143f790a21dbf6deeeaa4529efe
VongphetPSV <64593791+VongphetPSV@users.noreply.github.com>
2022-05-30 13:52:17 +0700

    Services Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Header from "./components/Header";
 import ParticlesBackground from "./components/ParticlesBackground";
 import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
          <Header />
          <ParticlesBackground />
          <AboutMe />
+         <Services />
       </div>
    );
 }
